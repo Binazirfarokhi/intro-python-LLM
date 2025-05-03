69551117036cdef91c43a22db5d9dd8bd872e8b8
Binazir <binazir.farokhi@gmail.com>
2025-05-03 16:50:34 -0700

    practice

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ face-recognition
 easy access to get data from web
 AI for data-sceience 
 input( ) box is promt 
+# print show something in the console
+top to botton and left to write 
+variables : save - like a box with a lable on it 
+<!-- str() converts a variable to a string (text) type in Python. -->
--- index.py
+++ index.py
@@ -1,1 +1,31 @@
 print("please run me on the browser by having me as a Python")
+failed_subjects = "6"
+print("number or use hammer  " + failed_subjects)
+# data-types : string - intigers- float 2.5 - boolean - 
+# you can not put numbers inside the 
+print(type(1.45))
+stringToNumber = 2
+print("this str can conver a number to a string  : " + str(stringToNumber))
+# we have int() : make it to the number 
+# we have float () make it to the decimal 2.4 
+# we have str() make it to the string 
+# we vall these them cast : cast them to str or int or float 
+# datatyoes and typecasting 
+# basic arithmetic : reyazi 
+a=3
+b=2
+print('addition', a+b)
+print('division (float)' , a/b)
+print('division (floor)' , a//b)
+# exponent mishe tavan 
+print('exponent: '            , a**b) 
+msg = " we have a cool stuff here "
+print(msg+msg)
+print(msg,msg)
+print(msg.upper())
+print(msg.capitalize())
+print(msg.title())
+print(len(msg))
+print(msg.count('w'))
+# slicing : getting a part of string and give it to us
+print(msg[5])
